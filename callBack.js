7474f7b55a4a31207bfe93a031a9551628e2103b
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-10-05 11:06:15 -0400

    Create callBack.js

---
+++ callBack.js
@@ -0,0 +1,8 @@
+console.log("Before timeout: " + new Date());
+
+function f() {
+    console.log("After timeout: " + new Date());
+}
+setTimeout(f, 60 * 1000); // one minute
+console.log("I happen after setTimeout!");
+console.log("Me too!");
